6dca5477a13db4977f7feb148bd28ba1be66c9e6
SM-Ahmed <121966317+SM-Ahmed@users.noreply.github.com>
2023-04-25 22:51:52 +0100

    Update __init__.py

--- PyGMINBayesOpt/__init__.py
+++ PyGMINBayesOpt/__init__.py
@@ -1,3 +1,4 @@
 from .training_generator import training_generator, gen_bounds, gen_training, gen_response, join_bounds, cleanup
 from .bayesopt import bayesopt, read_lowestEI, read_lowestGP, prep_one_singlepoint, prep_topo_batch, run_topo_batch, extract_topo_batch, gen_next_training, gen_next_response, prep_next_run
 from .analysis import analyse_runs
+from .gmin import gmin, count_lgbfs
